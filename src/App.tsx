d5c3c6518bba3fb8208eb3642982a92d9bebbb1a
MateusRanzani <mateusranzani@hotmail.com>
2022-12-22 15:01:09 -0300

    Update project

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import React from "react";
+import MenuButton from "./components/MenuButton";
 import "./styles/global.css";
 
 function App() {
   return (
     <div className="App">
       <div className="menuTopBar">
-        <div className="buttonsMenuTopBar">
-
+        <div className="menuTopBarArea">
+          <MenuButton textToButton="ABOUT ME"/>
+          <MenuButton textToButton="CONTACT"/>
+          <MenuButton textToButton="PROJECTS"/>
         </div>
       </div>
-      <div className="bodyPage">
-        Hello world!
-      </div>
+      <div className="bodyPage">Hello world!</div>
     </div>
   );
 }
